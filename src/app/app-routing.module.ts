e6c084e827bcde066818f3929774fcc13edd1f0f
Ashok Poudel <ashokpoudel34@gmail.com>
2023-02-04 13:39:42 +0530

    2nd Day changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",component:ContactListComponent},
+  {path: "add",component:ContactAddComponent},
+  {path: "edit",component:ContactEditComponent},
+  {path: "details",component:ContactDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
